f7e4bdbe04d4d04cec4a3a71276cf5cfca57c3f0
mounivelaga <mounivelaga@gmail.com>
2026-06-08 14:47:22 -0400

    Transformation data file code created

---
+++ src/transform.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from logger import logger
+
+
+def transform_data(data):
+    logger.info("Transformation Started")
+
+    df = pd.json_normalize(data)
+
+    required_columns = [
+        "employee_id",
+        "name",
+        "department",
+        "salary",
+        "location.city",
+        "location.country",
+    ]
+
+    for col in required_columns:
+        if col not in df.columns:
+            raise Exception(f"{col} column missing")
+
+    df["salary"] = df["salary"].fillna(0)
+
+    df["name"] = df["name"].str.title()
+
+    logger.info("Transformation completed")
+
+    return df
