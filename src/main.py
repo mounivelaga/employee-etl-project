82a2c2566f4dee48fd11d3624968288aae1f73e8
mounivelaga <mounivelaga@gmail.com>
2026-06-09 22:49:49 -0400

    main.py code updated

--- src/main.py
+++ src/main.py
@@ -4,4 +4,11 @@ from extract import extract_data
 from transform import transform_data
 from load import load_data
 
-with open("")
+with open("config.json") as file:
+    config = json.load(file)
+
+data = extract_data(config["input_file"])
+df = transform_data(data)
+load_data(df, config)
+
+print("ETL pipeline completed sucessfully")
