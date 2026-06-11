f8bf46ff847fd1492e4ffbb4add78b3ec66773f9
mounivelaga <mounivelaga@gmail.com>
2026-06-10 23:23:28 -0400

    extract.py updated

--- src/extract.py
+++ src/extract.py
@@ -3,10 +3,10 @@ from logger import logger
 
 
 def extract_data(file_path):
-    logger.info("Extraction Started")
+    logger.info("Extraction Started..")
 
     with open(file_path, "r") as file:
         data = json.load(file)
-    logger.info("Extraction Completed")
+    logger.info("Extraction Completed.")
 
     return data
